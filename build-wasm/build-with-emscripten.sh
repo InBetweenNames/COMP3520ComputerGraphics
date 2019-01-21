e962253562418b87ee3143212651442754038fdd
Shane Peelar <lookatyouhacker@gmail.com>
2019-01-21 13:05:53 -0500

    Support Emscripten builds using WebAssembly

---
+++ build-wasm/build-with-emscripten.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+if [[ -z $(which emcmake) ]]; then
+  echo "No emcmake detected.  Is Emscripten installed and in your PATH?"
+  echo "Remember to follow the Emscripten SDK instructions."
+  exit 1
+fi
+
+emcmake cmake ..
+cmake --build .
